5be9cb7838693f360dfebaf235e1154f74e7c9d3
Ciapa <markus@ciapa.io>
2017-06-30 14:41:12 +0200

    Fix BCard and Parse

--- OpenNos.GameObject/Buff/BCard.cs
+++ OpenNos.GameObject/Buff/BCard.cs
@@ -79,7 +79,7 @@ namespace OpenNos.GameObject
                         {
                             switch (SubType)
                             {
-                                case 20:
+                                case 2:
                                     EventHelper.Instance.RunEvent(new EventContainer((session as MapMonster).MapInstance, EventActionType.SPAWNMONSTERS, summonParameters));
                                     break;
                                 default:
