8b4c6683a2957b631dca03b079002675ae335086
Ciapa <markus@ciapa.io>
2017-07-11 16:31:48 +0200

    Fix Group HP Bug(Gitlab #1848)

--- OpenNos.GameObject/Networking/ServerManager.cs
+++ OpenNos.GameObject/Networking/ServerManager.cs
@@ -1268,7 +1268,7 @@ namespace OpenNos.GameObject
                         foreach (ClientSession session in groupMembers.GetAllItems())
                         {
                             session.SendPacket(session.Character.GeneratePinit());
-                            session.Character.Group.Characters.ForEach(s => session.SendPacket(s.Character.GenerateStat()));
+                            //session.Character.Group.Characters.ForEach(s => session.SendPacket(s.Character.GenerateStat()));
                             session.SendPackets(session.Character.GeneratePst());
                         }
                     }
