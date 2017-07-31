c19bf2549ae53f426c51de089b532ede17bf0603
Ciapa <markus@ciapa.io>
2017-08-01 01:44:01 +0200

    Fix one of my retarded Merge errors. Should boost performance quite a bit

--- OpenNos.GameObject/Networking/ServerManager.cs
+++ OpenNos.GameObject/Networking/ServerManager.cs
@@ -1379,18 +1379,6 @@ namespace OpenNos.GameObject
                 }
             });
 
-            Observable.Interval(TimeSpan.FromMilliseconds(400)).Subscribe(x =>
-            {
-                foreach (var map in _mapinstances)
-                {
-                    if (!map.Value.IsSleeping)
-                    {
-                        Parallel.ForEach(map.Value.Npcs, npc => { npc.StartLife(); });
-                        Parallel.ForEach(map.Value.Monsters, monster => { monster.StartLife(); });
-                    }
-                }
-            });
-
             CommunicationServiceClient.Instance.SessionKickedEvent += OnSessionKicked;
             CommunicationServiceClient.Instance.MessageSentToCharacter += OnMessageSentToCharacter;
             CommunicationServiceClient.Instance.FamilyRefresh += OnFamilyRefresh;
