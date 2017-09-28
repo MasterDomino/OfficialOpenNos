f8a007fccc564bceb5d4bd96c7ad8417c283234b
MasterDomino <mrdominogrm@gmail.com>
2017-09-28 19:10:19 +0200

    fix a small mistake in batch scripts(derp)

--- run_release(64bit).bat
+++ run_release(64bit).bat
@@ -1,8 +1,8 @@
-start "Master.Server" /D "%~dp0OpenNos.Master.Server\bin\x64\Release\" "OpenNos.Master.Server.exe"
+start %~dp0OpenNos.Master.Server\bin\x64\Release\OpenNos.Master.Server.exe
 timeout 10
-start "Login" /D "%~dp0OpenNos.Login\bin\x64\Release\" "OpenNos.Login.exe"
+start %~dp0OpenNos.Login\bin\x64\Release\OpenNos.Login.exe
 timeout 5
 :: edit to have wanted amount of world servers,
 :: dont forget to wait about 5 seconds before starting next world server
-start "World" /D "%~dp0OpenNos.World\bin\x64\Release\" "OpenNos.World.exe"
+start "%~dp0OpenNos.World\bin\x64\Release\OpenNos.World.exe
 exit
