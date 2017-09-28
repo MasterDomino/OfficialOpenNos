a54d215bb1d325cbd0c7719106c9101b083dc846
MasterDomino <mrdominogrm@gmail.com>
2017-09-28 19:15:11 +0200

    change an order of bootup sequence

--- run_release(AnyCPU).bat
+++ run_release(AnyCPU).bat
@@ -1,8 +1,8 @@
 start %~dp0OpenNos.Master.Server\bin\Release\OpenNos.Master.Server.exe
 timeout 10
-start %~dp0OpenNos.Login\bin\Release\OpenNos.Login.exe
-timeout 5
 :: edit to have wanted amount of world servers,
 :: dont forget to wait about 5 seconds before starting next world server
 start %~dp0OpenNos.World\bin\Release\OpenNos.World.exe
+timeout 20
+start %~dp0OpenNos.Login\bin\Release\OpenNos.Login.exe
 exit
