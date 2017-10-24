8b6af7228bace76f3b54a216e9b0016e93640050
Dominik <mrdominogrm@gmail.com>
2017-10-24 12:23:01 +0200

    last unneeded package removed

--- OpenNos.DAL.EF/Migrations/201710231715320_Aphrodite67.cs
+++ OpenNos.DAL.EF/Migrations/201710231715320_Aphrodite67.cs
@@ -1,8 +1,7 @@
 namespace OpenNos.DAL.EF.Migrations
 {
-    using System;
     using System.Data.Entity.Migrations;
-    
+
     public partial class Aphrodite67 : DbMigration
     {
         public override void Up()
@@ -12,7 +11,7 @@ namespace OpenNos.DAL.EF.Migrations
             DropIndex("dbo.ShellEffect", new[] { "ItemInstance_Id" });
             DropColumn("dbo.ShellEffect", "ItemInstance_Id");
         }
-        
+
         public override void Down()
         {
             AddColumn("dbo.ShellEffect", "ItemInstance_Id", c => c.Guid());
