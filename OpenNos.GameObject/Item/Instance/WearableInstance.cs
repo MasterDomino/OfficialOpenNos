08182af2978acf920bfdccc2ab18037785eab4c1
Markus Kostrzewski <ciapa@opennos.io>
2017-10-26 19:51:31 +0200

    Add MaxCellonLevel Restriction

--- OpenNos.GameObject/Item/Instance/WearableInstance.cs
+++ OpenNos.GameObject/Item/Instance/WearableInstance.cs
@@ -295,11 +295,13 @@ namespace OpenNos.GameObject
                     case 1026:
                         return; //no data known, not implemented in the client at all right now
 
-                                //dataIndex = 9;
-                                //break;
+                        //dataIndex = 9;
+                        //break;
                 }
 
-                short[][] minimumData = new short[][] {
+                if (Item.MaxCellonLvl < dataIndex)
+                {
+                    short[][] minimumData = new short[][] {
                     new short[] { 30, 50, 5, 8, 0, 0 },                 //lv1
                     new short[] { 120, 150, 14, 16, 0, 0 },             //lv2
                     new short[] { 220, 280, 22, 28, 0, 0 },             //lv3
@@ -311,7 +313,7 @@ namespace OpenNos.GameObject
                     new short[] { 1100, 1100, 110, 110, 14, 22 },       //lv9
                     new short[] { 0, 0, 0, 0, 0, 0 }                    //lv10 (NOT EXISTING!)
                 };
-                short[][] maximumData = new short[][] {
+                    short[][] maximumData = new short[][] {
                     new short[] { 100, 150, 10, 15, 0, 0 },          //lv1
                     new short[] { 200, 250, 20, 25, 0, 0 },          //lv1
                     new short[] { 300, 330, 28, 35, 0, 0 },          //lv1
@@ -324,52 +326,53 @@ namespace OpenNos.GameObject
                     new short[] { 0, 0, 0, 0, 0, 0 }            //lv10 (NOT EXISTING!)
                 };
 
-                short[] generateOption()
-                {
-                    byte option = 0;
-                    if (dataIndex < 5)
+                    short[] generateOption()
                     {
-                        option = (byte)ServerManager.Instance.RandomNumber(0, 4);
+                        byte option = 0;
+                        if (dataIndex < 5)
+                        {
+                            option = (byte)ServerManager.Instance.RandomNumber(0, 4);
+                        }
+                        else
+                        {
+                            option = (byte)ServerManager.Instance.RandomNumber(0, 6);
+                        }
+
+                        if (CellonOptions.Any(s => s.Type == (CellonOptionType)option))
+                        {
+                            return new short[] { -1, -1 };
+                        }
+
+                        return new short[] { option, (short)ServerManager.Instance.RandomNumber(minimumData[dataIndex][option], maximumData[dataIndex][option] + 1) };
+                    }
+
+                    short[] value = generateOption();
+                    Logger.LogUserEvent("OPTION", session.GenerateIdentity(), $"[OptionItem]Serial: {EquipmentSerialId} Type: {value[0]} Value: {value[1]}");
+                    if (value[0] != -1)
+                    {
+                        CellonOptionDTO cellonOptionDTO = new CellonOptionDTO()
+                        {
+                            EquipmentSerialId = EquipmentSerialId,
+                            Level = (byte)(dataIndex + 1),
+                            Type = (CellonOptionType)value[0],
+                            Value = value[1]
+                        };
+
+                        CellonOptions.Add(cellonOptionDTO);
+
+                        session.SendPacket(session.Character.GenerateSay(string.Format(Language.Instance.GetMessageFromKey("OPTION_SUCCESS"), Rare), 12));
+                        session.SendPacket(UserInterfaceHelper.Instance.GenerateMsg(string.Format(Language.Instance.GetMessageFromKey("OPTION_SUCCESS"), Rare), 0));
+                        session.CurrentMapInstance?.Broadcast(StaticPacketHelper.GenerateEff(UserType.Player, CharacterId, 3005), session.Character.PositionX, session.Character.PositionY);
+                        session.SendPacket("shop_end 1");
                     }
                     else
                     {
-                        option = (byte)ServerManager.Instance.RandomNumber(0, 6);
+                        session.SendPacket(session.Character.GenerateSay(string.Format(Language.Instance.GetMessageFromKey("OPTION_FAIL"), Rare), 12));
+                        session.SendPacket(UserInterfaceHelper.Instance.GenerateMsg(string.Format(Language.Instance.GetMessageFromKey("OPTION_FAIL"), Rare), 0));
+                        session.SendPacket("shop_end 1");
                     }
-
-                    if (CellonOptions.Any(s => s.Type == (CellonOptionType)option))
-                    {
-                        return new short[] { -1, -1 };
-                    }
-
-                    return new short[] { option, (short)ServerManager.Instance.RandomNumber(minimumData[dataIndex][option], maximumData[dataIndex][option] + 1) };
+                    session.Character.Inventory.RemoveItemAmount(cellonVNum);
                 }
-
-                short[] value = generateOption();
-                Logger.LogUserEvent("OPTION", session.GenerateIdentity(), $"[OptionItem]Serial: {EquipmentSerialId} Type: {value[0]} Value: {value[1]}");
-                if (value[0] != -1)
-                {
-                    CellonOptionDTO cellonOptionDTO = new CellonOptionDTO()
-                    {
-                        EquipmentSerialId = EquipmentSerialId,
-                        Level = (byte)(dataIndex + 1),
-                        Type = (CellonOptionType)value[0],
-                        Value = value[1]
-                    };
-
-                    CellonOptions.Add(cellonOptionDTO);
-
-                    session.SendPacket(session.Character.GenerateSay(string.Format(Language.Instance.GetMessageFromKey("OPTION_SUCCESS"), Rare), 12));
-                    session.SendPacket(UserInterfaceHelper.Instance.GenerateMsg(string.Format(Language.Instance.GetMessageFromKey("OPTION_SUCCESS"), Rare), 0));
-                    session.CurrentMapInstance?.Broadcast(StaticPacketHelper.GenerateEff(UserType.Player, CharacterId, 3005), session.Character.PositionX, session.Character.PositionY);
-                    session.SendPacket("shop_end 1");
-                }
-                else
-                {
-                    session.SendPacket(session.Character.GenerateSay(string.Format(Language.Instance.GetMessageFromKey("OPTION_FAIL"), Rare), 12));
-                    session.SendPacket(UserInterfaceHelper.Instance.GenerateMsg(string.Format(Language.Instance.GetMessageFromKey("OPTION_FAIL"), Rare), 0));
-                    session.SendPacket("shop_end 1");
-                }
-                session.Character.Inventory.RemoveItemAmount(cellonVNum);
             }
         }
 
