54a8d36be01a5d54c37b2162301a41c804ccc5b9
Markus Kostrzewski <ciapa@opennos.io>
2017-11-01 19:18:53 +0100

    Add the first NoFunctionItem that actually has a function

--- OpenNos.GameObject/Item/NoFunctionItem.cs
+++ OpenNos.GameObject/Item/NoFunctionItem.cs
@@ -33,6 +33,103 @@ namespace OpenNos.GameObject
         {
             switch (Effect)
             {
+                case 10:
+                    {
+                        switch (EffectValue)
+                        {
+                            case 1:
+                                if (session.Character.Inventory.CountItem(1036) < 1)
+                                {
+                                    return;
+                                }
+                                session.Character.Inventory.RemoveItemAmount(1036);
+                                if (ServerManager.Instance.RandomNumber() < 25)
+                                {
+                                    switch (ServerManager.Instance.RandomNumber(0, 2))
+                                    {
+                                        case 0:
+                                            session.Character.GiftAdd(1015, 1);
+                                            break;
+                                        case 1:
+                                            session.Character.GiftAdd(1016, 1);
+                                            break;
+                                    }
+                                }
+                                break;
+                            case 2:
+                                if (session.Character.Inventory.CountItem(1038) < 1)
+                                {
+                                    return;
+                                }
+                                session.Character.Inventory.RemoveItemAmount(1038);
+                                if (ServerManager.Instance.RandomNumber() < 25)
+                                {
+                                    switch (ServerManager.Instance.RandomNumber(0, 4))
+                                    {
+                                        case 0:
+                                            session.Character.GiftAdd(1031, 1);
+                                            break;
+                                        case 1:
+                                            session.Character.GiftAdd(1032, 1);
+                                            break;
+                                        case 2:
+                                            session.Character.GiftAdd(1033, 1);
+                                            break;
+                                        case 3:
+                                            session.Character.GiftAdd(1034, 1);
+                                            break;
+                                    }
+                                }
+                                break;
+                            case 3:
+                                if (session.Character.Inventory.CountItem(1037) < 1)
+                                {
+                                    return;
+                                }
+                                session.Character.Inventory.RemoveItemAmount(1037);
+                                if (ServerManager.Instance.RandomNumber() < 25)
+                                {
+                                    switch (ServerManager.Instance.RandomNumber(0, 17))
+                                    {
+                                        case 0:
+                                        case 1:
+                                        case 2:
+                                        case 3:
+                                        case 4:
+                                            session.Character.GiftAdd(1017, 1);
+                                            break;
+                                        case 5:
+                                        case 6:
+                                        case 7:
+                                        case 8:
+                                            session.Character.GiftAdd(1018, 1);
+                                            break;
+                                        case 9:
+                                        case 10:
+                                        case 11:
+                                            session.Character.GiftAdd(1019, 1);
+                                            break;
+                                        case 12:
+                                        case 13:
+                                            session.Character.GiftAdd(1020, 1);
+                                            break;
+                                        case 14:
+                                            session.Character.GiftAdd(1021, 1);
+                                            break;
+                                        case 15:
+                                            session.Character.GiftAdd(1022, 1);
+                                            break;
+                                        case 16:
+                                            session.Character.GiftAdd(1023, 1);
+                                            break;
+                                    }
+                                }
+                                break;
+                        }
+
+                        session.Character.GiftAdd(1014, (byte)ServerManager.Instance.RandomNumber(5,11));
+                    }
+                    break;
                 default:
                     Logger.Warn(string.Format(Language.Instance.GetMessageFromKey("NO_HANDLER_ITEM"), GetType()));
                     break;
