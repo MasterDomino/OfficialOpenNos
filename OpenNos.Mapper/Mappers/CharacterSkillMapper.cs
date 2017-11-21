df1dcf7ccd6da8846ff111e10e0d5b00ffc5b975
Ciapa <ciapa@opennos.io>
2017-11-21 22:03:21 +0100

    Add CharacterSkillMapper

---
+++ OpenNos.Mapper/Mappers/CharacterSkillMapper.cs
@@ -0,0 +1,27 @@
+﻿using OpenNos.DAL.EF;
+using OpenNos.Data;
+
+namespace OpenNos.Mapper.Mappers
+{
+    public class CharacterSkillMapper
+    {
+        public CharacterSkillMapper()
+        {
+
+        }
+
+        public void ToCharacterSkillDTO(CharacterSkill input, CharacterSkillDTO output)
+        {
+            output.CharacterId = input.CharacterId;
+            output.Id = input.Id;
+            output.SkillVNum = input.SkillVNum;
+        }
+
+        public void ToCharacterSkill(CharacterSkillDTO input, CharacterSkill output)
+        {
+            output.CharacterId = input.CharacterId;
+            output.Id = input.Id;
+            output.SkillVNum = input.SkillVNum;
+        }
+    }
+}
