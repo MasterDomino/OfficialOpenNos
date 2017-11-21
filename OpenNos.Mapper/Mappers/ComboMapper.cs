fe3117b17cd3e483f7dee8fef8e8d94c9e8a8cdb
Ciapa <ciapa@opennos.io>
2017-11-21 22:03:29 +0100

    Add ComboMapper

---
+++ OpenNos.Mapper/Mappers/ComboMapper.cs
@@ -0,0 +1,31 @@
+﻿using OpenNos.DAL.EF;
+using OpenNos.Data;
+
+namespace OpenNos.Mapper.Mappers
+{
+    public class ComboMapper
+    {
+        public ComboMapper()
+        {
+
+        }
+
+        public void ToComboDTO(Combo input, ComboDTO output)
+        {
+            output.Animation = input.Animation;
+            output.ComboId = input.ComboId;
+            output.Effect = input.Effect;
+            output.Hit = input.Hit;
+            output.SkillVNum = input.SkillVNum;
+        }
+
+        public void ToCombo(ComboDTO input, Combo output)
+        {
+            output.Animation = input.Animation;
+            output.ComboId = input.ComboId;
+            output.Effect = input.Effect;
+            output.Hit = input.Hit;
+            output.SkillVNum = input.SkillVNum;
+        }
+    }
+}
