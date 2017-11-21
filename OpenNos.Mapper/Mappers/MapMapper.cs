87f1af4300f10c4db749a05b67b6e390fc9dbe14
Ciapa <ciapa@opennos.io>
2017-11-21 22:05:22 +0100

    Add MapMapper

---
+++ OpenNos.Mapper/Mappers/MapMapper.cs
@@ -0,0 +1,32 @@
+﻿using OpenNos.DAL.EF;
+using OpenNos.Data;
+using System;
+
+namespace OpenNos.Mapper.Mappers
+{
+    public class MapMapper
+    {
+        public MapMapper()
+        {
+
+        }
+
+        public void ToMapDTO(Map input, MapDTO output)
+        {
+            output.Data = input.Data;
+            output.MapId = input.MapId;
+            output.Music = input.Music;
+            output.Name = input.Name;
+            output.ShopAllowed = input.ShopAllowed;
+        }
+
+        public void ToMap(MapDTO input, Map output)
+        {
+            output.Data = input.Data;
+            output.MapId = input.MapId;
+            output.Music = input.Music;
+            output.Name = input.Name;
+            output.ShopAllowed = input.ShopAllowed;
+        }
+    }
+}
