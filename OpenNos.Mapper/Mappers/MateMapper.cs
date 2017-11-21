cc923233822c0e6c29dc01e147fbc597e813f204
Ciapa <ciapa@opennos.io>
2017-11-21 22:06:08 +0100

    Add MateMapper

---
+++ OpenNos.Mapper/Mappers/MateMapper.cs
@@ -0,0 +1,61 @@
+﻿using OpenNos.DAL.EF;
+using OpenNos.DAL.EF.Entities;
+using OpenNos.Data;
+using System;
+
+namespace OpenNos.Mapper.Mappers
+{
+    public class MateMapper
+    {
+        public MateMapper()
+        {
+
+        }
+
+        public void ToMateDTO(Mate input, MateDTO output)
+        {
+            output.Attack = input.Attack;
+            output.CanPickUp = input.CanPickUp;
+            output.CharacterId = input.CharacterId;
+            output.Defence = input.Defence;
+            output.Direction = input.Direction;
+            output.Experience = input.Experience;
+            output.Hp = input.Hp;
+            output.IsSummonable = input.IsSummonable;
+            output.IsTeamMember = input.IsTeamMember;
+            output.Level = input.Level;
+            output.Loyalty = input.Loyalty;
+            output.MapX = input.MapX;
+            output.MapY = input.MapY;
+            output.MateId = input.MateId;
+            output.MateType = input.MateType;
+            output.Mp = input.Mp;
+            output.Name = input.Name;
+            output.NpcMonsterVNum = input.NpcMonsterVNum;
+            output.Skin = input.Skin;
+        }
+
+        public void ToMate(MateDTO input, Mate output)
+        {
+            output.Attack = input.Attack;
+            output.CanPickUp = input.CanPickUp;
+            output.CharacterId = input.CharacterId;
+            output.Defence = input.Defence;
+            output.Direction = input.Direction;
+            output.Experience = input.Experience;
+            output.Hp = input.Hp;
+            output.IsSummonable = input.IsSummonable;
+            output.IsTeamMember = input.IsTeamMember;
+            output.Level = input.Level;
+            output.Loyalty = input.Loyalty;
+            output.MapX = input.MapX;
+            output.MapY = input.MapY;
+            output.MateId = input.MateId;
+            output.MateType = input.MateType;
+            output.Mp = input.Mp;
+            output.Name = input.Name;
+            output.NpcMonsterVNum = input.NpcMonsterVNum;
+            output.Skin = input.Skin;
+        }
+    }
+}
