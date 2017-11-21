c4dd0b1c1ea48c848a9d5f5902f14ea79a625329
Ciapa <ciapa@opennos.io>
2017-11-21 22:06:23 +0100

    Add MinilandObjectMapper

---
+++ OpenNos.Mapper/Mappers/MinilandObjectMapper.cs
@@ -0,0 +1,42 @@
+﻿using OpenNos.DAL.EF;
+using OpenNos.Data;
+using System;
+
+namespace OpenNos.Mapper.Mappers
+{
+    public class MinilandObjectMapper
+    {
+        public MinilandObjectMapper()
+        {
+
+        }
+
+        public void ToMinilandObjectDTO(MinilandObject input, MinilandObjectDTO output)
+        {
+            output.CharacterId = input.CharacterId;
+            output.ItemInstanceId = input.ItemInstanceId;
+            output.Level1BoxAmount = input.Level1BoxAmount;
+            output.Level2BoxAmount = input.Level2BoxAmount;
+            output.Level3BoxAmount = input.Level3BoxAmount;
+            output.Level4BoxAmount = input.Level4BoxAmount;
+            output.Level5BoxAmount = input.Level5BoxAmount;
+            output.MapX = input.MapX;
+            output.MapY = input.MapY;
+            output.MinilandObjectId = input.MinilandObjectId;
+        }
+
+        public void ToMinilandObject(MinilandObjectDTO input, MinilandObject output)
+        {
+            output.CharacterId = input.CharacterId;
+            output.ItemInstanceId = input.ItemInstanceId;
+            output.Level1BoxAmount = input.Level1BoxAmount;
+            output.Level2BoxAmount = input.Level2BoxAmount;
+            output.Level3BoxAmount = input.Level3BoxAmount;
+            output.Level4BoxAmount = input.Level4BoxAmount;
+            output.Level5BoxAmount = input.Level5BoxAmount;
+            output.MapX = input.MapX;
+            output.MapY = input.MapY;
+            output.MinilandObjectId = input.MinilandObjectId;
+        }
+    }
+}
