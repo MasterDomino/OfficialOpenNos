38254e5daa63d3ca7dd6e9a174ecfc6d36f51b71
Ciapa <ciapa@opennos.io>
2017-11-21 22:03:46 +0100

    Add FamilyCharacterMapper

---
+++ OpenNos.Mapper/Mappers/FamilyCharacterMapper.cs
@@ -0,0 +1,35 @@
+﻿using OpenNos.DAL.EF;
+using OpenNos.Data;
+
+namespace OpenNos.Mapper.Mappers
+{
+    public class FamilyCharacterMapper
+    {
+        public FamilyCharacterMapper()
+        {
+
+        }
+
+        public void ToFamilyCharacterDTO(FamilyCharacter input, FamilyCharacterDTO output)
+        {
+            output.Authority = input.Authority;
+            output.CharacterId = input.CharacterId;
+            output.DailyMessage = input.DailyMessage;
+            output.Experience = input.Experience;
+            output.FamilyCharacterId = input.FamilyCharacterId;
+            output.FamilyId = input.FamilyId;
+            output.Rank = input.Rank;
+        }
+
+        public void ToFamilyCharacter(FamilyCharacterDTO input, FamilyCharacter output)
+        {
+            output.Authority = input.Authority;
+            output.CharacterId = input.CharacterId;
+            output.DailyMessage = input.DailyMessage;
+            output.Experience = input.Experience;
+            output.FamilyCharacterId = input.FamilyCharacterId;
+            output.FamilyId = input.FamilyId;
+            output.Rank = input.Rank;
+        }
+    }
+}
