b3eafe823555986ac0f235b49367f4d185cf31a4
Ciapa <ciapa@opennos.io>
2017-11-21 22:05:31 +0100

    Add MapMonsterMapper

---
+++ OpenNos.Mapper/Mappers/MapMonsterMapper.cs
@@ -0,0 +1,38 @@
+﻿using OpenNos.DAL.EF;
+using OpenNos.Data;
+using System;
+
+namespace OpenNos.Mapper.Mappers
+{
+    public class MapMonsterMapper
+    {
+        public MapMonsterMapper()
+        {
+
+        }
+
+        public void ToMapMonsterDTO(MapMonster input, MapMonsterDTO output)
+        {
+            output.IsDisabled = input.IsDisabled;
+            output.IsMoving = input.IsMoving;
+            output.MapId = input.MapId;
+            output.MapMonsterId = input.MapMonsterId;
+            output.MapX = input.MapX;
+            output.MapY = input.MapY;
+            output.MonsterVNum = input.MonsterVNum;
+            output.Position = input.Position;
+        }
+
+        public void ToMapMonster(MapMonsterDTO input, MapMonster output)
+        {
+            output.IsDisabled = input.IsDisabled;
+            output.IsMoving = input.IsMoving;
+            output.MapId = input.MapId;
+            output.MapMonsterId = input.MapMonsterId;
+            output.MapX = input.MapX;
+            output.MapY = input.MapY;
+            output.MonsterVNum = input.MonsterVNum;
+            output.Position = input.Position;
+        }
+    }
+}
