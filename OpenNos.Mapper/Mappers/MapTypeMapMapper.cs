24ddfa8b5c9ff6b5a85c6d820740679352ec83d0
Ciapa <ciapa@opennos.io>
2017-11-21 22:05:52 +0100

    Add MapTypeMapMapper

---
+++ OpenNos.Mapper/Mappers/MapTypeMapMapper.cs
@@ -0,0 +1,26 @@
+﻿using OpenNos.DAL.EF;
+using OpenNos.Data;
+using System;
+
+namespace OpenNos.Mapper.Mappers
+{
+    public class MapTypeMapMapper
+    {
+        public MapTypeMapMapper()
+        {
+
+        }
+
+        public void ToMapTypeMapDTO(MapTypeMap input, MapTypeMapDTO output)
+        {
+            output.MapId = input.MapId;
+            output.MapTypeId = input.MapTypeId;
+        }
+
+        public void ToMapTypeMap(MapTypeMapDTO input, MapTypeMap output)
+        {
+            output.MapId = input.MapId;
+            output.MapTypeId = input.MapTypeId;
+        }
+    }
+}
