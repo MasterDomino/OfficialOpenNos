56ea56f7d7c4f29a16921f064916184ffe3fb131
Ciapa <ciapa@opennos.io>
2017-11-21 22:03:12 +0100

    Add CharacterRelationMapper

---
+++ OpenNos.Mapper/Mappers/CharacterRelationMapper.cs
@@ -0,0 +1,29 @@
+﻿using OpenNos.DAL.EF;
+using OpenNos.Data;
+
+namespace OpenNos.Mapper.Mappers
+{
+    public class CharacterRelationMapper
+    {
+        public CharacterRelationMapper()
+        {
+
+        }
+
+        public void ToCharacterRelationDTO(CharacterRelation input, CharacterRelationDTO output)
+        {
+            output.CharacterId = input.CharacterId;
+            output.CharacterRelationId = input.CharacterRelationId;
+            output.RelatedCharacterId = input.RelatedCharacterId;
+            output.RelationType = input.RelationType;
+        }
+
+        public void ToCharacterRelation(CharacterRelationDTO input, CharacterRelation output)
+        {
+            output.CharacterId = input.CharacterId;
+            output.CharacterRelationId = input.CharacterRelationId;
+            output.RelatedCharacterId = input.RelatedCharacterId;
+            output.RelationType = input.RelationType;
+        }
+    }
+}
