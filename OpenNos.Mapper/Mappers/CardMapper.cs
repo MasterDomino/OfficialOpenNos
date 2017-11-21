6cec8200757f116e0f8a595bea7e131d6d9f7170
Ciapa <ciapa@opennos.io>
2017-11-21 22:02:51 +0100

    Add CardMapper

---
+++ OpenNos.Mapper/Mappers/CardMapper.cs
@@ -0,0 +1,42 @@
+﻿using OpenNos.DAL.EF;
+using OpenNos.Data;
+using OpenNos.Domain;
+
+namespace OpenNos.Mapper.Mappers
+{
+    public class CardMapper
+    {
+        public CardMapper()
+        {
+
+        }
+
+        public void ToCardDTO(Card input, CardDTO output)
+        {
+            output.BuffType = input.BuffType;
+            output.CardId = input.CardId;
+            output.Delay = input.Delay;
+            output.Duration = input.Duration;
+            output.EffectId = input.EffectId;
+            output.Level = input.Level;
+            output.Name = input.Name;
+            output.Propability = input.Propability;
+            output.TimeoutBuff = input.TimeoutBuff;
+            output.TimeoutBuffChance = input.TimeoutBuffChance;
+        }
+
+        public void ToCard(CardDTO input, Card output)
+        {
+            output.BuffType = input.BuffType;
+            output.CardId = input.CardId;
+            output.Delay = input.Delay;
+            output.Duration = input.Duration;
+            output.EffectId = input.EffectId;
+            output.Level = input.Level;
+            output.Name = input.Name;
+            output.Propability = input.Propability;
+            output.TimeoutBuff = input.TimeoutBuff;
+            output.TimeoutBuffChance = input.TimeoutBuffChance;
+        }
+    }
+}
