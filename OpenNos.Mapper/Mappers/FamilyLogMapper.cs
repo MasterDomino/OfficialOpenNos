e78330093c674054cb019df86e8715768c5b7148
Ciapa <ciapa@opennos.io>
2017-11-21 22:03:55 +0100

    Add FamilyLogMapper

---
+++ OpenNos.Mapper/Mappers/FamilyLogMapper.cs
@@ -0,0 +1,31 @@
+﻿using OpenNos.DAL.EF;
+using OpenNos.Data;
+
+namespace OpenNos.Mapper.Mappers
+{
+    public class FamilyLogMapper
+    {
+        public FamilyLogMapper()
+        {
+
+        }
+
+        public void ToFamilyLogDTO(FamilyLog input, FamilyLogDTO output)
+        {
+            output.FamilyId = input.FamilyId;
+            output.FamilyLogData = input.FamilyLogData;
+            output.FamilyLogId = input.FamilyLogId;
+            output.FamilyLogType = input.FamilyLogType;
+            output.Timestamp = input.Timestamp;
+        }
+
+        public void ToFamilyLog(FamilyLogDTO input, FamilyLog output)
+        {
+            output.FamilyId = input.FamilyId;
+            output.FamilyLogData = input.FamilyLogData;
+            output.FamilyLogId = input.FamilyLogId;
+            output.FamilyLogType = input.FamilyLogType;
+            output.Timestamp = input.Timestamp;
+        }
+    }
+}
