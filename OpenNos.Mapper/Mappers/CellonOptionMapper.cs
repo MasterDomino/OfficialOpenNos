2dee1f4b7bfec5dafc60fb2f938e924904a2aa79
Ciapa <ciapa@opennos.io>
2017-11-21 22:03:00 +0100

    Add CellonOptionMapper

---
+++ OpenNos.Mapper/Mappers/CellonOptionMapper.cs
@@ -0,0 +1,32 @@
+﻿
+using OpenNos.DAL.EF;
+using OpenNos.Data;
+
+namespace OpenNos.Mapper.Mappers
+{
+    public class CellonOptionMapper
+    {
+        public CellonOptionMapper()
+        {
+
+        }
+
+        public void ToCellonOptionDTO(CellonOption input, CellonOptionDTO output)
+        {
+            output.CellonOptionId = input.CellonOptionId;
+            output.EquipmentSerialId = input.EquipmentSerialId;
+            output.Level = input.Level;
+            output.Type = input.Type;
+            output.Value = input.Value;
+        }
+
+        public void ToCellonOption(CellonOptionDTO input, CellonOption output)
+        {
+            output.CellonOptionId = input.CellonOptionId;
+            output.EquipmentSerialId = input.EquipmentSerialId;
+            output.Level = input.Level;
+            output.Type = input.Type;
+            output.Value = input.Value;
+        }
+    }
+}
