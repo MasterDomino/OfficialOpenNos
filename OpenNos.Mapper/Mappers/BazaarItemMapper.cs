59f8a2941d5e346f3d4252756da54716a6441688
Ciapa <ciapa@opennos.io>
2017-11-21 22:02:19 +0100

    Add BazaarItemMapper

---
+++ OpenNos.Mapper/Mappers/BazaarItemMapper.cs
@@ -0,0 +1,39 @@
+﻿using OpenNos.DAL.EF;
+using OpenNos.Data;
+
+namespace OpenNos.Mapper.Mappers
+{
+    public class BazaarItemMapper
+    {
+        public BazaarItemMapper()
+        {
+
+        }
+
+        public void ToBazaarItemDTO(BazaarItem input, BazaarItemDTO output)
+        {
+            output.Amount = input.Amount;
+            output.BazaarItemId = input.BazaarItemId;
+            output.DateStart = input.DateStart;
+            output.Duration = input.Duration;
+            output.IsPackage = input.IsPackage;
+            output.ItemInstanceId = input.ItemInstanceId;
+            output.MedalUsed = input.MedalUsed;
+            output.Price = input.Price;
+            output.SellerId = input.SellerId;
+        }
+
+        public void ToBazaarItem(BazaarItemDTO input, BazaarItem output)
+        {
+            output.Amount = input.Amount;
+            output.BazaarItemId = input.BazaarItemId;
+            output.DateStart = input.DateStart;
+            output.Duration = input.Duration;
+            output.IsPackage = input.IsPackage;
+            output.ItemInstanceId = input.ItemInstanceId;
+            output.MedalUsed = input.MedalUsed;
+            output.Price = input.Price;
+            output.SellerId = input.SellerId;
+        }
+    }
+}
