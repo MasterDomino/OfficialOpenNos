ab5f8aecadff07881679db3d1b08c18ac3712642
Ciapa <ciapa@opennos.io>
2017-11-21 22:04:38 +0100

    Add GeneralLogMapper

---
+++ OpenNos.Mapper/Mappers/GeneralLogMapper.cs
@@ -0,0 +1,35 @@
+﻿using OpenNos.DAL.EF;
+using OpenNos.Data;
+
+namespace OpenNos.Mapper.Mappers
+{
+    public class GeneralLogMapper
+    {
+        public GeneralLogMapper()
+        {
+
+        }
+
+        public void ToGeneralLogDTO(GeneralLog input, GeneralLogDTO output)
+        {
+            output.AccountId = input.AccountId;
+            output.CharacterId = input.CharacterId;
+            output.IpAddress = input.IpAddress;
+            output.LogData = input.LogData;
+            output.LogId = input.LogId;
+            output.LogType = input.LogType;
+            output.Timestamp = input.Timestamp;
+        }
+
+        public void ToGeneralLog(GeneralLogDTO input, GeneralLog output)
+        {
+            output.AccountId = input.AccountId;
+            output.CharacterId = input.CharacterId;
+            output.IpAddress = input.IpAddress;
+            output.LogData = input.LogData;
+            output.LogId = input.LogId;
+            output.LogType = input.LogType;
+            output.Timestamp = input.Timestamp;
+        }
+    }
+}
