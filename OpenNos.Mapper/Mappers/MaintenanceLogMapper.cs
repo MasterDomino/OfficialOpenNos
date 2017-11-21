5851cbc7f7edec36d39b90a898452a51429c0f7d
Ciapa <ciapa@opennos.io>
2017-11-21 22:05:15 +0100

    Add MaintenanceLogMapper

---
+++ OpenNos.Mapper/Mappers/MaintenanceLogMapper.cs
@@ -0,0 +1,30 @@
+﻿using OpenNos.DAL.EF;
+using OpenNos.Data;
+using System;
+
+namespace OpenNos.Mapper.Mappers
+{
+    public class MaintenanceLogMapper
+    {
+        public MaintenanceLogMapper()
+        {
+
+        }
+
+        public void ToMaintenanceLogDTO(MaintenanceLog input, MaintenanceLogDTO output)
+        {
+            output.DateEnd = input.DateEnd;
+            output.DateStart = input.DateStart;
+            output.LogId = input.LogId;
+            output.Reason = input.Reason;
+        }
+
+        public void ToMaintenanceLog(MaintenanceLogDTO input, MaintenanceLog output)
+        {
+            output.DateEnd = input.DateEnd;
+            output.DateStart = input.DateStart;
+            output.LogId = input.LogId;
+            output.Reason = input.Reason;
+        }
+    }
+}
