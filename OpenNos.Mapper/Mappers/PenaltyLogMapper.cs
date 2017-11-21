3f40436bbea2e0e42513843e363d972abf3639a3
Ciapa <ciapa@opennos.io>
2017-11-21 22:06:31 +0100

    Add PenaltyLogMapper

--- OpenNos.Mapper/Mappers/PenaltyLogMapper.cs
+++ OpenNos.Mapper/Mappers/PenaltyLogMapper.cs
@@ -1,10 +1,34 @@
-﻿using System;
+﻿using OpenNos.DAL.EF;
+using OpenNos.Data;
+
 namespace OpenNos.Mapper.Mappers
 {
     public class PenaltyLogMapper
     {
         public PenaltyLogMapper()
         {
+        }
+
+        public void ToPenaltyLogDTO(PenaltyLog input, PenaltyLogDTO output)
+        {
+            output.AccountId = input.AccountId;
+            output.AdminName = input.AdminName;
+            output.DateEnd = input.DateEnd;
+            output.DateStart = input.DateStart;
+            output.Penalty = input.Penalty;
+            output.PenaltyLogId = input.PenaltyLogId;
+            output.Reason = input.Reason;
+        }
+
+        public void ToPenaltyLog(PenaltyLogDTO input, PenaltyLog output)
+        {
+            output.AccountId = input.AccountId;
+            output.AdminName = input.AdminName;
+            output.DateEnd = input.DateEnd;
+            output.DateStart = input.DateStart;
+            output.Penalty = input.Penalty;
+            output.PenaltyLogId = input.PenaltyLogId;
+            output.Reason = input.Reason;
         }
     }
 }
