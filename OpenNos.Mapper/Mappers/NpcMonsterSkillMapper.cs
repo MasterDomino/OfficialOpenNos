33078fde7211edf2ff6b0e9741d6963805e1cdca
Ciapa <ciapa@opennos.io>
2017-11-22 11:38:09 +0100

    Add more Mappers, need to be added to base mapper class
    Added NpcMonsterMapper to RespawnMapTypeMapper, still missing RollGeneratedItemMapper to WearableInstanceMapper

---
+++ OpenNos.Mapper/Mappers/NpcMonsterSkillMapper.cs
@@ -0,0 +1,28 @@
+﻿using OpenNos.DAL.EF;
+using OpenNos.Data;
+
+namespace OpenNos.Mapper.Mappers
+{
+    public class NpcMonsterSkillMapper
+    {
+        public NpcMonsterSkillMapper()
+        {
+        }
+
+        public void ToNpcMonsterSkillDTO(NpcMonsterSkill input, NpcMonsterSkillDTO output)
+        {
+            output.NpcMonsterSkillId = input.NpcMonsterSkillId;
+            output.NpcMonsterVNum = input.NpcMonsterVNum;
+            output.Rate = input.Rate;
+            output.SkillVNum = input.SkillVNum;
+        }
+
+        public void ToNpcMonsterSkill(NpcMonsterSkillDTO input, NpcMonsterSkill output)
+        {
+            output.NpcMonsterSkillId = input.NpcMonsterSkillId;
+            output.NpcMonsterVNum = input.NpcMonsterVNum;
+            output.Rate = input.Rate;
+            output.SkillVNum = input.SkillVNum;
+        }
+    }
+}
